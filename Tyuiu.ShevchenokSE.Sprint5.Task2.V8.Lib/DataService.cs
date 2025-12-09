c912ea0bf53fb74b80f89cb3d54e9a223a1058ba
Nagibator2007 <shevchenokse@outlook.com>
2025-12-09 22:54:35 +0500

    fix

--- Tyuiu.ShevchenokSE.Sprint5.Task2.V8.Lib/DataService.cs
+++ Tyuiu.ShevchenokSE.Sprint5.Task2.V8.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.ShevchenokSE.Sprint5.Task2.V8.Lib
 {
     public class DataService : ISprint5Task2V8
@@ -7,6 +8,7 @@ namespace Tyuiu.ShevchenokSE.Sprint5.Task2.V8.Lib
         {
             string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
             string str = "";
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int count = 0, j = 0; j < matrix.GetLength(1); j++, count++)
@@ -21,11 +23,11 @@ namespace Tyuiu.ShevchenokSE.Sprint5.Task2.V8.Lib
                     }
                     if (count != 2)
                     {
-                        str += matrix[i, j].ToString() + "\t";
+                        str += matrix[i, j].ToString() + ";";
                     }
                     else
                     {
-                        str += matrix[i, j].ToString() + "\n";
+                        str += matrix[i, j].ToString() + ";";
                     }
                 }
                 File.WriteAllText(path, str);
