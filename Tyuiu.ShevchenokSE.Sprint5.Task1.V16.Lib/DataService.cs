3f3b2d1f589004f2a2639383a9302af033d8bdb5
Nagibator2007 <shevchenokse@outlook.com>
2025-12-09 22:08:15 +0500

    lib fix

--- Tyuiu.ShevchenokSE.Sprint5.Task1.V16.Lib/DataService.cs
+++ Tyuiu.ShevchenokSE.Sprint5.Task1.V16.Lib/DataService.cs
@@ -1,29 +1,21 @@
-﻿using System.Globalization;
-using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.ShevchenokSE.Sprint5.Task1.V16.Lib
 {
     public class DataService : ISprint5Task1V16
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask1.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
 
-            using (StreamWriter writer = new StreamWriter(path))
+            while (startValue <= stopValue)
             {
-                for (int x = startValue; x <= stopValue; x++)
-                {
-                    double y = Math.Sin(x) + (2.0 * x) / 3.0 - Math.Cos(x) * 4.0 * x;
-                    y = Math.Round(y, 2);
-                    string strY = y.ToString("F2", CultureInfo.InvariantCulture);
-                    if (x != stopValue)
-                    {
-                        writer.WriteLine(strY);
-                    }
-                    else
-                    {
-                        writer.Write(strY);
-                    }
-                }
+                double result = Math.Sin(startValue) + (2 * startValue) / 3 - Math.Cos(startValue) * 4 * startValue;
+                if (double.IsInfinity(result) || double.IsNaN(result))
+                    result = 0;
+                result = Math.Round(result, 2);
+
+                File.AppendAllText(path, $"{result.ToString()}\n");
+                startValue++;
             }
 
             return path;
