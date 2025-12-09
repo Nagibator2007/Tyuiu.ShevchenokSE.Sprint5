68dd4146cf4d2c4a833c877205a66a952815d0fc
Nagibator2007 <shevchenokse@outlook.com>
2025-12-09 21:04:55 +0500

    Add project files.

---
+++ Tyuiu.ShevchenokSE.Sprint5.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task0.V15.Lib
+{
+    public class DataService : ISprint5Task0V15
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
+            double y = x / (Math.Pow(x, 3) + 2.0);
+            y = Math.Round(y, 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint5.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShevchenokSE.Sprint5.Task0.V15.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\sergejbabidzonov\Desktop\source\repos\Tyuiu.ShevchenokSE.Sprint5\Tyuiu.ShevchenokSE.Sprint5.Task0.V15\bin\Debug\net8.0\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
