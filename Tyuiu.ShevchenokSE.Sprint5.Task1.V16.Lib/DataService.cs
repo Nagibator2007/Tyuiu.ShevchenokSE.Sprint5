a1783ff29acfd18b499576784f2ba3692ed70d3e
Nagibator2007 <shevchenokse@outlook.com>
2025-12-09 22:06:56 +0500

    lib fix

--- Tyuiu.ShevchenokSE.Sprint5.Task1.V16.Lib/DataService.cs
+++ Tyuiu.ShevchenokSE.Sprint5.Task1.V16.Lib/DataService.cs
@@ -1,21 +1,29 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.ShevchenokSE.Sprint5.Task1.V16.Lib
 {
     public class DataService : ISprint5Task1V16
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask1.txt";
 
-            while (startValue <= stopValue)
+            using (StreamWriter writer = new StreamWriter(path))
             {
-                double result = Math.Sin(startValue) + (2 * startValue) / 3 - Math.Cos(startValue) * 4 * startValue;
-                if (double.IsInfinity(result) || double.IsNaN(result))
-                    result = 0;
-                result = Math.Round(result, 2);
-
-                File.AppendAllText(path, $"{result.ToString()}\n");
-                startValue++;
+                for (int x = startValue; x <= stopValue; x++)
+                {
+                    double y = Math.Sin(x) + (2.0 * x) / 3.0 - Math.Cos(x) * 4.0 * x;
+                    y = Math.Round(y, 2);
+                    string strY = y.ToString("F2", CultureInfo.InvariantCulture);
+                    if (x != stopValue)
+                    {
+                        writer.WriteLine(strY);
+                    }
+                    else
+                    {
+                        writer.Write(strY);
+                    }
+                }
             }
 
             return path;
