a74daf9f0e8baafed1d7ab321d06e6e535c2b059
Nagibator2007 <shevchenokse@outlook.com>
2025-12-10 17:59:35 +0500

    Task5

---
+++ Tyuiu.ShevchenokSE.Sprint5.Task5.V18.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task5.V18.Lib
+{
+    public class DataService : ISprint5Task5V18
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double product = 1;
+            bool foundTwoDigitNumber = false;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (double.TryParse(line.Trim(), out double number))
+                    {
+                        if ((number >= 10 && number <= 99) || (number <= -10 && number >= -99))
+                        {
+                            number = Math.Round(number, 3);
+                            product *= number;
+                            foundTwoDigitNumber = true;
+                        }
+                    }
+                }
+            }
+            if (!foundTwoDigitNumber)
+            {
+                return 0;
+            }
+            return Math.Round(product, 3);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint5.Task5.V18/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ShevchenokSE.Sprint5.Task5.V18.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task5.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Шевченок С.Е   | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич |  СМАРТб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask5V18.txt в котором есть набор*");
+            Console.WriteLine("* значений. Найти произведение всех двузначных чисел в файле.             *");
+            Console.WriteLine("* Полученный результат вывести на консоль. У вещественных значений        *");
+            Console.WriteLine("* округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V18.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Произведение всех двузначных чисел: " + res);
+            Console.ReadKey();
+        }
+    }
+}
