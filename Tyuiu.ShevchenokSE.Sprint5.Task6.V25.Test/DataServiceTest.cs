7bcf4deab10c4dec4b6b3406a621e7b6aa5ab270
Nagibator2007 <shevchenokse@outlook.com>
2025-12-10 21:57:24 +0500

    Task6!

---
+++ Tyuiu.ShevchenokSE.Sprint5.Task6.V25.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShevchenokSE.Sprint5.Task6.V25.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task6.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask6V25.txt");
+            var res = ds.LoadFromDataFile(path);
+            int wait = 6;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint5.Task6.V25/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.ShevchenokSE.Sprint5.Task6.V25.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task6.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Шевченок С.Е   | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич |  СМАРТб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask6V25.txt в котором есть      *");
+            Console.WriteLine("* набор символьных данных. Найти количество знаков восклицания            *");
+            Console.WriteLine("* в заданной строке.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask6V25.txt");
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Количество знаков восклицания в заданной строке = " + res);
+        }
+    }
+}
