daf3a4d6b3ca2409729881c4bcb37704449b9956
Nagibator2007 <shevchenokse@outlook.com>
2025-12-10 17:13:03 +0500

    Task4

---
+++ Tyuiu.ShevchenokSE.Sprint5.Task4.V2.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task4.V2.Lib
+{
+    public class DataService : ISprint5Task4V2
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strx = File.ReadAllText(path);
+            double x = Double.Parse(strx, CultureInfo.InvariantCulture);
+            double numerator = Math.Cos(x) + 3 * x;
+            double denominator = 0.25 * x;
+            double res = numerator / denominator + 4 - 2.1 * x;
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint5.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.ShevchenokSE.Sprint5.Task4.V2.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task4.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V2.txt";
+            DataService ds = new DataService();
+            double res = ds.LoadFromDataFile(path);
+            Assert.IsTrue(!double.IsNaN(res) && !double.IsInfinity(res));
+        }
+    }
+}
