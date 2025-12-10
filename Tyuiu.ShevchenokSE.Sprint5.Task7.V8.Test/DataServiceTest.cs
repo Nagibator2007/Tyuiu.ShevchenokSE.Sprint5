cd09488d9be0edbc4a7a4c8a5100db0368fef5d5
Nagibator2007 <shevchenokse@outlook.com>
2025-12-10 21:33:07 +0500

    Task7

---
+++ Tyuiu.ShevchenokSE.Sprint5.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShevchenokSE.Sprint5.Task7.V8.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task7.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\User\AppData\Local\Temp\OutPutDataFileTask7V8.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            Assert.IsTrue(fileInfo.Exists);
+
+        }
+    }
+}
---
+++ Tyuiu.ShevchenokSE.Sprint5.Task7.V8/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ShevchenokSE.Sprint5.Task7.V8.Lib;
+namespace Tyuiu.ShevchenokSE.Sprint5.Task7.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Шевченок С.Е   | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Шевченок Сергей Евгеньевич |  СМАРТб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask7V8.txt, в котором есть      *");
+            Console.WriteLine("* набор символьных данных.Заменить все заглавные русские буквы на         *");
+            Console.WriteLine("* строчные. Полученный результат сохранить в файл                         *");
+            Console.WriteLine("* OutPutDataFileTask7V8.txt.                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = $@"C:\Users\User\Desktop\DataSprint5\InPutDataFileTask7V8.txt";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string PathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine($"Данные находятся в файле");
+            Console.WriteLine(PathSaveFile);
+        }
+    }
+}
